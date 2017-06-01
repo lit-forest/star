5eee6c2483e91e0408d25de660a4f7ef299364f0
sylvenas <sylvenas@outlook.com>
2017-06-01 22:17:45 +0800

    修改样式

--- src/App.js
+++ src/App.js
@@ -11,29 +11,13 @@ class App extends Component {
         <div className='header-section'>
           <ul className='nav'>
             <li className='nav-item'><a href='/' className='nav-item-a'>All Products</a></li>
-          </ul>
-          <ul>
             <li className='nav-item'><a href='/' className='nav-item-a'>UI Kits</a></li>
-          </ul>
-          <ul>
             <li className='nav-item'><a href='/' className='nav-item-a'>Wireframe Kits</a></li>
-          </ul>
-          <ul>
             <li className='nav-item'><a href='/' className='nav-item-a'>For Sketch</a></li>
-          </ul>
-          <ul>
             <li className='nav-item'><a href='/' className='nav-item-a'>Icons</a></li>
-          </ul>
-          <ul>
             <li className='nav-item'><a href='/' className='nav-item-a'>Fonts</a></li>
-          </ul>
-          <ul>
             <li className='nav-item'><a href='/' className='nav-item-a'>Mockups</a></li>
-          </ul>
-          <ul>
             <li className='nav-item'><a href='/' className='nav-item-a'>Presentation</a></li>
-          </ul>
-          <ul>
             <li className='nav-item'><a href='/' className='nav-item-a'>Icons</a></li>
           </ul>
         </div>
